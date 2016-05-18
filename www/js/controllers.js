297d2bebe6463485db03852b005fc33ab5b531d5
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-05-18 18:00:32 -0400

    Create an array and change color

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  
+
+  $scope.comics = [
+    {
+      title: "Mafalda",
+      author: "Quino",
+      cover: "mafalda.jpg",
+      year: 1990
+    },
+    {
+      title: "Calvin and Hobbes",
+      author: "dsfsdf sdfsdf",
+      cover: "calvin.png",
+      year:1990
+    },
+    {
+      title: "Pentaus",
+      author: "dsfdsf sdfsdaf",
+      cover: "charlie.png",
+      year: 1907
+    }
+  ];
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
